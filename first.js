6a68d6b1730106149600224580f35bc838de06b6
saransh <saransh.singh14@gmail.com>
2020-01-26 17:41:35 +0530

    created few lines of JS to get familiar with JS
    will work further on Java script now

---
+++ first.js
@@ -0,0 +1,12 @@
+// var firstName= prompt("what is your first name?");
+// var lastName= prompt("what is your last name?");
+// var age= prompt("what is your age?");
+// console.log("your full name is "+ firstName + " " + lastName );
+// console.log("your are "+ age + " years old" );
+
+
+
+var age= prompt("what is your age?");
+var days= 365;
+var lived= 365* age;
+console.log("you have been alive for " +lived + "days");
