798245f69a827bb1962c84376f34c7566412c0ff
saransh <saransh.singh14@gmail.com>
2020-01-28 10:42:05 +0530

    learnt if , else conditions
    gained info. on if , else , if else condition 
    and also on while loop

--- first.js
+++ first.js
@@ -5,8 +5,49 @@
 // console.log("your are "+ age + " years old" );
 
 
+//
+// var age= prompt("what is your age?");
+// var days= 365;
+// var lived= 365* age;
+// console.log("you have been alive for " +lived + "days");
 
-var age= prompt("what is your age?");
-var days= 365;
-var lived= 365* age;
-console.log("you have been alive for " +lived + "days");
+//
+// var age= Number(prompt("what is your age?"));
+// if(age < 0)  {
+//   console.log("come back when you are out of womb!");
+// }
+// if(age === 21) {
+//   console.log("Happpy '21' st birthday! ");
+// }
+// if(age % 2 !==0) {
+//   console.log("your age is odd!");
+// }
+//
+
+//
+// var num= -10 ;
+// while(num <= 20) {
+//   console.log(num);
+// num++ ;
+// }
+
+// var num= 10 ;
+// while(num <= 40) {
+//   console.log(num);
+//   num+=2;
+// }
+
+// var num= 300 ;
+// while(num <= 333) {
+//
+// if(num % 2 !== 0) {
+//   console.log(num);
+// }
+// num+=1;
+// }
+
+// var ask= prompt("are we there yet?");
+// while(ask !=="yes" && ask !=="yeah"){
+//   var ask= prompt("are we there yet?");
+// }
+// alert("great!");
